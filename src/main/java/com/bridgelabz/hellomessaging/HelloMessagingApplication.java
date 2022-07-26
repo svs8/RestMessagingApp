9e2aef0d21d0b7c003a702bf009a218bb29b9a97
svs8 <surakshithvittalshetty@gmail.com>
2022-07-26 23:37:31 +0530

    Created an REST API Messaging App

---
+++ src/main/java/com/bridgelabz/hellomessaging/HelloMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellomessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMessagingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloMessagingApplication.class, args);
+    }
+
+}
